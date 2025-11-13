fdddfff3e2624259a8520926f46d5c07b4d1bbc1
vernere <erner.etola@metropolia.fi>
2025-11-13 13:41:39 +0200

    Updated operation choosing to its own function

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,35 +9,19 @@ public class App {
         Logger logger = Logger.getLogger(App.class.getName());
         Scanner input = new Scanner(System.in);
         if (logger != null) {
+
             logger.info("Enter first number: ");
             double num1 = input.nextDouble();
             logger.info("Enter second number: ");
             double num2 = input.nextDouble();
+
             logger.info("Choose operation (1: Add, 2: Subtract, 3: Multiply, 4: Divide): ");
             int operation = input.nextInt();
-            double value = 0;            
-            switch (operation) {
-                case 1:
-                    value = addValues(num1, num2);
-                    break;
-                case 2:
-                    value = subtractValues(num1, num2);
-                    break;
-                case 3:
-                    value = multiplyValues(num1, num2);
-                    break;
-                case 4:
-                    value = divideValues(num1, num2);
-                    break;
-                default:
-                    logger.info("Invalid operation");
-            }
-            double result;
-            result = value;
+            double result = chooseOperation(operation, num1, num2, logger) ;
+
             logger.info(() -> String.format("The result is: %f", result));
 
             input.close();
-
         }
 
     }
@@ -57,4 +41,21 @@ public class App {
     public static double subtractValues(double num1, double num2) {
         return num1 - num2;
     }
+
+    public static double chooseOperation(int operation, double num1, double num2, Logger logger) {
+        switch (operation) {
+            case 1:
+                return addValues(num1, num2);
+            case 2:
+                return subtractValues(num1, num2);
+            case 3:
+                return multiplyValues(num1, num2);
+            case 4:
+                return divideValues(num1, num2);
+            default:
+                logger.info("Invalid operation");
+                return 0.0;
+        }
+    }
+    
 }
